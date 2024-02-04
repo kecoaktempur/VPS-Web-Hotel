9d254f37dde3f4b52cf3ab2668526622f51e2446
athraz <athrazan2004@gmail.com>
2024-02-04 18:09:32 +0700

    feat: routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// user
+Route::get('/', function () {return view('home');});
+Route::get('/home', [PageController::class, 'home'])->name('home');
+Route::get('/type', [TypeController::class, 'indexUser'])->name('type.index.user');
+Route::post('/type', [TypeController::class, 'available'])->name('type.available');
+Route::get('/type/{id}', [TypeController::class, 'showUser'])->name('type.show.user');
+
+// admin
+Route::get('/admin/login', [AuthController::class, 'login'])->name('login');
+Route::post('/admin/login', [AuthController::class, 'authenticate'])->name('login');
+Route::get('/admin/register', [AuthController::class, 'register'])->name('register');
+Route::post('/admin/logout', [AuthController::class, 'create'])->name('register');
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/admin/type', [TypeController::class, 'index'])->name('type.index.admin');
+Route::get('/admin/type/create', [TypeController::class, 'create'])->name('type.create');
+Route::post('/admin/type/store', [TypeController::class, 'store'])->name('type.store');
+Route::get('/admin/type/{id}', [TypeController::class, 'show'])->name('type.show.admin');
+Route::get('/admin/type/edit/{id}', [TypeController::class, 'edit'])->name('type.edit');
+Route::put('/admin/type/update/{id}', [TypeController::class, 'update'])->name('type.update');
+Route::delete('/admin/type/delete/{id}', [TypeController::class, 'destroy'])->name('type.destroy');
+
+Route::get('/admin/room', [RoomController::class, 'index'])->name('room');
+Route::get('/admin/room/create', [RoomController::class, 'create'])->name('room.create');
+Route::post('/admin/room/store', [RoomController::class, 'store'])->name('room.store');
+Route::get('/admin/room/{id}', [RoomController::class, 'show'])->name('room.show');
+Route::get('/admin/room/edit/{id}', [RoomController::class, 'edit'])->name('room.edit');
+Route::put('/admin/room/update/{id}', [RoomController::class, 'update'])->name('room.update');
+Route::delete('/admin/room/delete/{id}', [RoomController::class, 'destroy'])->name('room.destroy');
+
+Route::get('/admin/transaction', [TransactionController::class, 'index'])->name('transaction');
+Route::get('/admin/transaction/create', [TransactionController::class, 'create'])->name('transaction.create');
+Route::post('/admin/transaction/store', [TransactionController::class, 'store'])->name('transaction.store');
+Route::get('/admin/transaction/{id}', [TransactionController::class, 'show'])->name('transaction.show');
+Route::get('/admin/transaction/edit/{id}', [TransactionController::class, 'edit'])->name('transaction.edit');
+Route::put('/admin/transaction/update/{id}', [TransactionController::class, 'update'])->name('transaction.update');
+Route::delete('/admin/transaction/delete/{id}', [TransactionController::class, 'destroy'])->name('transaction.destroy');
+
+Route::get('/admin/notif', [NotificationController::class, 'index'])->name('notif');
+Route::get('/admin/notif/{id}', [NotificationController::class, 'show'])->name('notif.show');
+
+Route::get('/admin/photo', [PhotoController::class, 'index'])->name('photo');
+Route::get('/admin/photo/create', [PhotoController::class, 'create'])->name('photo.create');
+Route::post('/admin/photo/store', [PhotoController::class, 'store'])->name('photo.store');
+Route::delete('/admin/photo/delete/{id}', [PhotoController::class, 'destroy'])->name('photo.destroy');
