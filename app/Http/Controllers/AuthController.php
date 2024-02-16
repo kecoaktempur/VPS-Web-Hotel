00753bfdc46477d84cdee8e35de7b69c8890fe01
aptarr_ <apta.rasendriya@gmail.com>
2024-02-16 13:33:59 +0700

    feat: add login page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,26 +8,22 @@ class AuthController extends Controller
 {
     public function login()
     {
-
+        return view('admin.auth');
     }
 
     public function authenticate(Request $request)
     {
-
     }
 
     public function register()
     {
-
     }
 
     public function create(Request $request)
     {
-
     }
 
     public function logout(Request $request)
     {
-
     }
 }
---
+++ resources/views/admin/auth.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+<section class="bg-[#12182E]">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <div class="grid place-items-center">
+                    <img class="h-12 mb-5 mr-2" src="{{ asset('img/VPS_Logo_Dark.png') }}" alt="logo">
+                </div>
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
+                    Login Admin
+                </h1>
+                <form class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                        <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#3E9CC0] focus:border-[#3E9CC0] block w-full p-2.5" placeholder="Username" required="">
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#3E9CC0] focus:border-[#3E9CC0] block w-full p-2.5" required="">
+                    </div>
+                    <button type="submit" class="w-full text-white bg-[#3E9CC0] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Log
+                        In</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
