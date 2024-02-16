d7be297aafc5b2726a3e580a34567c163bd01392
kecoaktempur <triwan98@gmail.com>
2024-02-16 15:09:05 +0700

    style: minor patching

--- resources/views/admin/base-admin.blade.php
+++ resources/views/admin/base-admin.blade.php
@@ -4,59 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/icon/icon-vps.png">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/admin/base-admin.css">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <nav>
-        <a href="/home">
-            <img src="" alt="Logo VPS"></a>
-    </nav>
     <main>
         <div class="sidebar">
             <a href="" id="" class="tab">
                 <div class="tab-content">
-                    <i class="fa-solid fa-user"></i>
+                    <i class="fa-solid fa-bars-progress"></i>
                     <h3>Dashboard</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
             <a href="" id="" class="tab">
                 <div class="tab-content">
                     <i class="fa-solid fa-wallet"></i>
                     <h3>Transaksi</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
             <a href="" id="" class="tab">
                 <div class="tab-content">
                     <i class="fa-solid fa-file"></i>
                     <h3>Tipe</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
             <a href="" class="tab">
                 <div class="tab-content">
                     <i class="fa-solid fa-file-circle-check"></i>
                     <h3>Ruangan</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
             <a href="" class="tab" id="">
                 <div class="tab-content">
                     <i class="fa-solid fa-calendar"></i>
                     <h3>Transaksi</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
             <a href="" class="tab" id="tab">
                 <div class="tab-content">
-                    <i class="fa-solid fa-truck"></i>
-                    <h3>Transaksi</h3>
+                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                    <h3>Logout</h3>
                 </div>
-                <i class="fa-solid fa-check"></i>
             </a>
         </div>
         @yield('admin-content')
